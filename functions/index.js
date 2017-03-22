94232519966292c8c4a644485cce257c36301351
Philipp Gfeller <tuelsch@gmail.com>
2017-03-22 10:34:21 +0100

    correct functions

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,4 @@
 const functions = require('firebase-functions');
-const tools = require('firebase-tools');
-const marked = require('marked');
-const html = marked(readme, {sanitize: true});
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/functions/write-firebase-functions
