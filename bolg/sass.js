0e44f5ff200a1c75f114c7a1cf3a021fb39ee838
Philipp Gfeller <tuelsch@gmail.com>
2017-04-03 14:56:10 +0200

    Run now, heroku?

--- bolg/sass.js
+++ bolg/sass.js
@@ -2,19 +2,19 @@ const sass = require('node-sass');
 const writefile = require('./writefile');
 const crypto = require('crypto');
 
-module.exports = function () {
-  return new Promise(function (resolve, reject) {
+module.exports = function compileSass() {
+  return new Promise((resolve, reject) => {
     sass.render({
       file: 'src/styles/_post-index.scss',
       sourceMap: true,
 
-    }, function (err, result) {
+    }, (err, result) => {
       if (err) reject(err);
-      result.hash = crypto.createHash('md5').update(result.css).digest('hex');
-      result.webpath = `/css/bolg.${result.hash}.css`;
+      const r = result;
+      r.hash = crypto.createHash('md5').update(result.css).digest('hex');
+      r.webpath = `/css/bolg.${result.hash}.css`;
       writefile(`public/css/bolg.${result.hash}.css`, result.css).then(() => {
-        console.log(result.hash);
-        resolve(result);
+        resolve(r);
       });
       if (result.map) writefile('public/css/bolg.css.map', result.map);
     });
--- server/server.js
+++ server/server.js
@@ -1,36 +1,60 @@
 const express = require('express');
 const bolg = require('../bolg');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 // Serve the static files
+// app.use(favicon('public', 'favicon.ico'));
 app.use(express.static('public', { extensions: ['html'] }));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Listen for rebuild requests
 app.get('/rebuild', (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+  res.setHeader('Access-Control-Allow-Origin', 'localhost');
+
   bolg
     .rebuildAll()
     .then(bolg.rebuildIndex)
     .then(() => res.send({ message: 'Rebuild complete.' }))
     .catch(err => res.status(500).send(err.stack));
 });
+
 app.get('/rebuild/:id', (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+  res.setHeader('Access-Control-Allow-Origin', 'localhost');
+
   const operation = req.params.id === 'index'
     ? bolg.rebuildIndex()
     : bolg.rebuild(req.params.id);
+
   operation
     .then(bolg.rebuildIndex)
     .then(() => res.send({ message: 'Rebuild complete.' }))
     .catch(err => res.status(500).send(err.stack));
 });
 
-app.listen(2222, () => {
-  bolg.rebuildAll()
-    .then(bolg.rebuildIndex)
-    .then(() => {
-      /* eslint-disable no-console */
-      console.log('express server listening at http://localhost:2222');
-    });
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
+
+// error handler
+app.use((err, req, res) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
