11ad49c32359d07e95487e27651968cd9300e822
Philipp Gfeller <philipp.gfeller@maxomedia.ch>
2017-04-15 13:52:28 +0200

    Refactor the title into a getter method

--- src/models/Post.js
+++ src/models/Post.js
@@ -5,7 +5,6 @@ export default function (post = {}) {
 
   // Properties
   this.id = post.id || cuid();
-  this.title = post.title || '';
   this.created = post.created || Date.now();
   this.lastEdited = post.lastEdited || null;
   this.lastSaved = post.lastSaved || null;
@@ -15,13 +14,26 @@ export default function (post = {}) {
   this.markdown = post.markdown || '';
   this.images = post.images || [];
 
+  // Constants
   const ref = database.ref(`/posts/${this.id}`);
 
-  // Methods
+  /**
+   * Save the post to firebase
+   */
   this.set = () => {
     this.lastSaved = Date.now();
     return ref.set(JSON.parse(JSON.stringify(this))).then(snapshot => {
       return snapshot;
     });
   };
+
+  /**
+   * Get the post title, first line starting with a single #
+   */
+  Object.defineProperty(this, 'title', {
+    get() {
+      const title = this.markdown.match(/^# .+/gm);
+      return title ? title[0].replace('# ', '') : '';
+    },
+  });
 }
