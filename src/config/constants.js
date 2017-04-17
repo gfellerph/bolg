c4eaab8ad136189786d312f5bbb965d0c3153f07
Philipp Gfeller <tuelsch@gmail.com>
2017-04-17 21:49:16 +0200

    Add reverse geolocation service

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 export const states = {
   LOADING: 0,
   EDITING: 1,
@@ -6,4 +8,10 @@ export const states = {
   EDITING_OFFLINE: 4,
   SAVED_OFFLINE: 5,
   PUBLISHED: 6,
+};
+
+export const mapsAPIKey = 'AIzaSyBADvjevyMmDkHb_xjjh3FOltkO2Oa8iAQ';
+
+export function reverseGeocode(lat, lng) {
+  return axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${mapsAPIKey}`);
 }
