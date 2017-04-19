19663a3e89ac1b3a577ab41ede64e2a3fa949d85
Philipp Gfeller <tuelsch@gmail.com>
2017-04-19 20:20:35 +0200

    Update the map entry file

--- src/map.js
+++ src/map.js
@@ -1,8 +1,19 @@
-/* global google, document, window */
-window.addEventListener('load', function () {
-  window.initMap = () => {
-    const map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 4,
-    });
-  };
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue';
+import Map from '@/components/Map';
+import router from './config/router';
+import store from './config/store';
+import dateFormat from './filters/date-format';
+
+Vue.filter('dateFormat', dateFormat);
+Vue.config.productionTip = false;
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#map',
+  store,
+  router,
+  template: '<Map/>',
+  components: { Map },
 });
