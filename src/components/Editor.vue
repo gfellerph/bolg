88683dab9ba3cfb1cb5853638e3b3cd372ebb7d8
Philipp Gfeller <tuelsch@gmail.com>
2017-04-26 12:44:00 +0200

    Track cursor position, add bold and italic transforms

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -12,45 +12,89 @@
     <textarea
       id="markdown-editor"
       name="markdown-editor"
-      v-model="post.markdown"
-      ref="markdownEditor"
-      @keyup="keyup"
+      v-model="markdown"
+      @input="change"
+      ref="md"
+      @keydown="keydown"
       @scroll="scroll"
+      @keyup="cursorPositionChanged"
+      @click="cursorPositionChanged"
+      @focus="cursorPositionChanged"
     ></textarea>
+    <div class="mirror" ref="mirror" v-html="startToCursor"></div>
+    <div class="mirror" ref="mirrorRef" v-html="entireText"></div>
   </div>
 </template>
 
 <script>
-  let lastMarkdown = '';
+  let lastValue = '';
 
   export default {
     data() {
       return {
-        lastCursorPosition: 0,
+        markdown: this.value,
+        cursorPositionPercent: 0,
+        cursorLine: 0,
+        selectionStart: 0,
+        startToCursor: '',
+        entireText: '',
       };
     },
 
     props: {
-      post: Object,
-    },
-
-    computed: {
-      hasTitle() {
-        return !!this.post.title;
-      }
+      value: String,
     },
 
     methods: {
-      bolden() {
+      toggle(str, head, tail, start, end) {
+        let toggled = '';
+        const regex = new RegExp(`[${head}].+[${tail}]`, 'g');
+        const selected = str.slice(start, end);
 
+        if (!selected) return str;
+
+        if (str.substring(start, end).match(regex)) {
+          toggled = [str.slice(0, start), str.slice(start, end).replace(head, '').replace(tail, ''), str.slice(end, str.length)].join('');
+        } else {
+          toggled = [str.slice(0, start), head, str.slice(start, end), tail, str.slice(end, str.length)].join('')
+        }
+
+        return toggled;
+      },
+      bolden(e) {
+        const md = this.$refs.md;
+        const start = md.selectionStart;
+        const end = md.selectionEnd;
+        this.markdown = this.toggle(this.markdown, '**', '**', start, end);
+        this.change();
+        this.$nextTick(() => {
+          md.setSelectionRange(start, end);
+          md.focus();
+          console.log(start, end);
+        });
       },
       pizzaparty() {
-
+        const md = this.$refs.md;
+        this.markdown = this.toggle(this.markdown, '_', '_', md.selectionStart, md.selectionEnd);
+        this.keyup();
       },
-      keyup() {
-        if (this.post.markdown !== lastMarkdown) {
-          lastMarkdown = this.post.markdown;
-          this.$emit('change', this.post.markdown);
+      change() {
+        if (this.markdown !== lastValue) {
+          lastValue = this.markdown;
+          this.$emit('input', this.markdown);
+        }
+      },
+      keydown(event) {
+        // Save on ctrl+s
+        if (event.key === 's' && event.ctrlKey) {
+          event.preventDefault();
+          this.$emit('save');
+        }
+
+        // Toggle bold on ctrl+b
+        if (event.key === 'b' && event.ctrlKey) {
+          event.preventDefault();
+          this.bolden();
         }
       },
       scroll(event) {
@@ -58,15 +102,63 @@
         const percent = target.scrollTop / (target.scrollHeight - target.clientHeight);
         this.$emit('scroll', percent);
       },
-      toggleClippy() {
-
+      toggleClippy() {},
+      cursorPositionChanged(event) {
+        this.startToCursor = this.markdown.substring(0, this.$refs.md.selectionStart)
+          .replace(/&/g, "&amp;")
+          .replace(/</g, "&lt;")
+          .replace(/>/g, "&gt;")
+          .replace(/\n/g, '<br>');
+        this.entireText = this.markdown
+          .replace(/&/g, "&amp;")
+          .replace(/</g, "&lt;")
+          .replace(/>/g, "&gt;")
+          .replace(/\n/g, '<br>');
+        this.$nextTick(function () {
+          const textareaHeight = this.$refs.mirrorRef.scrollHeight;
+          const mirrorHeight = this.$refs.mirror.scrollHeight;
+          let percent = mirrorHeight / textareaHeight;
+          if (percent > 0.95) percent = 1;
+          this.$emit('scroll', percent);
+        });
       },
+    },
+
+    created() {
+      this.$watch('value', function () {
+        this.markdown = this.value;
+      });
+    },
+
+    mounted() {
+      this.selectionStart = this.$refs.md ? this.$refs.md.selectionStart : 0;
     },
   };
 </script>
 
 <style lang="scss" scoped>
   @import 'src/styles/_variables';
+
+  .mirror {
+    position: absolute;
+    top: 37.31px;
+    left: 0;
+    right: 0;
+    height: auto;
+    width: 100%;
+    padding: 0 $golden-rem / 2;
+    z-index: -1;
+    visibility: hidden;
+  }
+
+  .mirror,
+  textarea {
+    font-family: monospace;
+    font-size: 12px;
+    line-height: 1.3em;
+    letter-spacing: 0;
+    overflow-y: scroll;
+  }
 
   .controls {
     display: flex;
@@ -100,9 +192,11 @@
   }
 
   .editor {
+    position: relative;
     display: flex;
     flex-direction: column;
     width: 100%;
+    overflow: hidden;
   }
 
   textarea {
