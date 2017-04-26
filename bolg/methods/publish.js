7c4e3d3f8fd3c491c8559b5bf0e31d2bd6284e6d
Philipp Gfeller <tuelsch@gmail.com>
2017-04-26 12:40:44 +0200

    Splitting that up is nonesense

--- bolg/methods/publish.js
+++
@@ -1,7 +0,0 @@
-const firebase = require('../config/firebase');
-const helpers = require('../helpers');
-const webpackManifest = require('../config/webpack.manifest.json');
-
-module.exports = function publish(id) {
-
-};
