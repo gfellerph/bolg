6723fdc3028971e9de9682d1d89c7cb66a3194ac
Philipp Gfeller <tuelsch@gmail.com>
2017-08-07 11:29:12 +0200

    Temporary no stories

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
         <router-link class="button" to="/createpost">Create post</router-link>
         <router-link class="button" to="/map">Map</router-link>
         <router-link class="button" to="/bookmark">Bookmark</router-link>
-        <router-link class="button" to="/createstory">Create story</router-link>
+        <!-- <router-link class="button" to="/createstory">Create story</router-link>-->
       </nav>
       <img class="logo" :src="logoURL" alt="">
       <profile></profile>
