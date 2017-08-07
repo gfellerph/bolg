c39e81a758c8f40a30502b70c102b67e6fee3513
Philipp Gfeller <tuelsch@gmail.com>
2017-08-07 11:39:05 +0200

    Fix second merge conflict

--- src/components/EditPost.vue
+++ src/components/EditPost.vue
@@ -54,19 +54,12 @@
   const renderer = new origMarked.Renderer();
 
   renderer.image = (href, title, text) => {
-<<<<<<< HEAD
     const srcset = '';
     const hrefAttr = href ? ` src="${href}"` : '';
     const titleAttr = title ? ` title="${title}"` : '';
     const altAttr = text ? ` alt="${text}"` : '';
     const srcsetAttr = srcset ? ` srcset="${srcset}"` : '';
     return `<img${hrefAttr}${titleAttr}${altAttr}${srcsetAttr}>`;
-=======
-    const srcset = sizes.map(size => `${getThumbUrl(href, size)} ${size.width}w`).join(',');
-    const titleAttr = title ? `title="${title}"` : '';
-    console.log(`<img src="${href}" ${titleAttr} alt="${text}" srcset="">`);
-    return `<img src="${href}" ${titleAttr} alt="${text}" srcset="">`;
->>>>>>> stories
   }
 
   export default {
