8e1963be0915d74b745f5dd0dcf7113ea55060f9
Philipp Gfeller <tuelsch@gmail.com>
2017-08-21 12:46:36 +0200

    Load hd images on mediu zoom

--- src/gallery.js
+++ src/gallery.js
@@ -1,3 +1,22 @@
 import mediumZoom from '@/config/medium-zoom';
 
-window.addEventListener('DOMContentLoaded', mediumZoom);
+window.addEventListener('DOMContentLoaded', () => {
+  const zoomer = mediumZoom();
+
+  zoomer.addEventListeners('show', (event) => {
+    const img = event.currentTarget;
+
+    if (!img.hasAttribute('data-hd')) return;
+
+    img.setAttribute('data-lowres', img.getAttribute('src'));
+    img.setAttribute('src', img.getAttribute('data-hd'));
+  });
+
+  zoomer.addEventListeners('hide', (event) => {
+    const img = event.currentTarget;
+
+    if (!img.hasAttribute('data-lowres')) return;
+
+    img.setAttribute('src', img.getAttribute('data-lowres'));
+  });
+});
