1401e1e14d3fb55e7348fc2351387d933f1d6bdd
Philipp Gfeller <tuelsch@gmail.com>
2017-09-03 16:26:00 +0200

    Add notify api endpoint

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import publishAllApi from '@/server/api/publish-all';
 import publishApi from '@/server/api/publish';
 import unpublishApi from '@/server/api/unpublish';
 import unsubscribe from '@/server/api/unsubscribe';
+import notifySubscribers from '@/server/api/notify-subscribers';
 
 const app = express();
 
@@ -30,6 +31,7 @@ app.get('/publish', publishAllApi);
 app.get('/publish/:id', publishApi);
 app.get('/unpublish/:id', unpublishApi);
 app.get('/unsubscribe/:id', unsubscribe);
+app.get('/notifysubscribers/:id', notifySubscribers);
 
 // catch 404 and forward to error handler
 // TODO: Find a way to manage errors
