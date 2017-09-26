7c333b1ea5ffa23334d81e125a87841c91c719ae
Philipp Gfeller <tuelsch@gmail.com>
2017-09-27 00:06:34 +0800

    Update paths and remove dupl. plugins from front prod

--- config/front.prod.config.js
+++ config/front.prod.config.js
@@ -2,18 +2,15 @@ const merge = require('webpack-merge');
 const webpack = require('webpack');
 const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const ManifestPlugin = require('webpack-manifest-plugin');
-const baseConfig = require('./base.config');
 const frontConfig = require('./front.config');
-const paths = require('./paths');
 
-const devConfig = merge(baseConfig, frontConfig, {
+const devConfig = merge(frontConfig, {
   output: {
-    filename: '[name].[chunkhash].js',
+    filename: 'js/[name].[chunkhash].js',
   },
   plugins: [
     new ExtractTextPlugin({
-      filename: '/css/[name].[chunkhash].css',
+      filename: 'css/[name].[chunkhash].css',
       allChunks: true,
     }),
     new webpack.optimize.UglifyJsPlugin({
@@ -23,10 +20,6 @@ const devConfig = merge(baseConfig, frontConfig, {
       sourceMap: true,
     }),
     new OptimizeCSSPlugin(),
-    new ManifestPlugin({
-      fileName: paths.manifest,
-      basePath: paths.assets,
-    }),
   ],
   devtool: '#source-map',
 });
