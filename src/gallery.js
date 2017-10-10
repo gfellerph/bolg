3e8abe393f50e4b9f473f7e7346d87aeffa7559f
Philipp Gfeller <tuelsch@gmail.com>
2017-10-10 13:27:51 +0800

    Fix #13, Upgrade medium zoom and use new hd feature

--- src/gallery.js
+++ src/gallery.js
@@ -1,24 +1,5 @@
 import mediumZoom from '@/config/medium-zoom';
 
 window.addEventListener('DOMContentLoaded', () => {
-  const zoomer = mediumZoom();
-
-  if (!zoomer) return;
-
-  zoomer.addEventListeners('shown', (event) => {
-    const img = event.currentTarget;
-
-    if (!img.hasAttribute('data-hd')) return;
-
-    img.setAttribute('data-lowres', img.getAttribute('src'));
-    img.setAttribute('src', img.getAttribute('data-hd'));
-  });
-
-  zoomer.addEventListeners('hide', (event) => {
-    const img = event.currentTarget;
-
-    if (!img.hasAttribute('data-lowres')) return;
-
-    img.setAttribute('src', img.getAttribute('data-lowres'));
-  });
+  mediumZoom();
 });
