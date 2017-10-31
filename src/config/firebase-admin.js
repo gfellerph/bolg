363ec93bfc2c979ffc4b35c39e591c6fb585f274
Philipp Gfeller <tuelsch@gmail.com>
2017-10-31 19:18:09 +0800

    Fix google functions

--- src/config/firebase-admin.js
+++ src/config/firebase-admin.js
@@ -5,6 +5,7 @@ const serviceAccount = require('./firebase-admin-key.json');
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: 'https://bolg-d1098.firebaseio.com',
+  storageBucket: 'bolg-d1098.appspot.com',
 });
 
 export default admin;
