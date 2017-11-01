a6096b0bb807d207a341694d1c646953a980324d
Philipp Gfeller <tuelsch@gmail.com>
2017-11-01 21:05:24 +0800

    Refactor bookmark code into a module, remove bookmark restore

--- src/post.js
+++ src/post.js
@@ -1,46 +1,12 @@
 import mediumZoom from '@/config/medium-zoom';
 import drawing from '@/modules/drawing';
-
-const documentHeight = () => Math.max(
-  document.body.scrollHeight,
-  document.body.offsetHeight,
-  document.documentElement.clientHeight,
-  document.documentElement.scrollHeight,
-  document.documentElement.offsetHeight,
-);
-
-function setBookmark() {
-  const html = document.documentElement;
-  const height = documentHeight();
-  const postMeta = document.querySelector('meta[property="id"]');
-  const postId = postMeta ? postMeta.getAttribute('content') : null;
-  const scrollPosition = window.pageYOffset || html.scrollTop;
-  const scrollPercent = scrollPosition / height;
-
-  if (!window.localStorage) return;
-
-  window.localStorage.setItem('bookmark', JSON.stringify({
-    scrollPercent,
-    url: window.location.href,
-    postId,
-  }));
-}
-
-function restoreBookmark() {
-  if (!window.localStorage) return;
-
-  const bookmark = JSON.parse(window.localStorage.getItem('bookmark'));
-  if (bookmark && window.location.href === bookmark.url && window.location.hash === 'bookmark') {
-    const height = documentHeight();
-    const offset = height * bookmark.scrollPercent;
-    if (window.scrollTo) window.scrollTo(0, offset);
-  }
-}
+import { setBookmark } from '@/modules/bookmark';
 
 if (window.location.href.indexOf('bolg.html') <= 0) {
-  window.onbeforeunload = setBookmark;
-  window.onunload = setBookmark;
-  window.addEventListener('load', restoreBookmark);
+  window.addEventListener('unload', () => {
+    console.log('unload');
+    setBookmark();
+  });
   window.addEventListener('DOMContentLoaded', () => {
     mediumZoom();
     drawing();
