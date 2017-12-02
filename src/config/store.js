f248e289fbd7e80002dea4ab7e019182f87d86d6
Philipp Gfeller <tuelsch@gmail.com>
2017-12-02 10:05:44 +0800

    Load vue locally, vuex needs to register

--- src/config/store.js
+++ src/config/store.js
@@ -1,10 +1,11 @@
 import 'es6-promise/auto';
+import Vue from 'vue';
 import Vuex from 'vuex';
 
 import authStore from 'src/stores/auth-store';
 import connectionStore from 'src/stores/connection-store';
 
-// Vue.use(Vuex);
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   strict: true,
