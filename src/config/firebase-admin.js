f42fdd9f00ee37085698a22221c30e4ab7cb0dd8
Philipp Gfeller <tuelsch@gmail.com>
2017-12-06 22:24:16 +0800

    Use .env for private keys

--- src/config/firebase-admin.js
+++ src/config/firebase-admin.js
@@ -2,6 +2,9 @@ import admin from 'firebase-admin';
 
 const serviceAccount = require('./firebase-admin-key.json');
 
+serviceAccount.private_key = process.env.FIREBASE_ADMIN_PRIVATE_KEY.replace(/\\n/g, '\n');
+serviceAccount.private_key_id = process.env.FIREBASE_ADMIN_PRIVATE_KEY_ID;
+
 if (admin.apps.length === 0) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
