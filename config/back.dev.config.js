0e891859082ebfa984ceb454833d6779cb76defd
Philipp Gfeller <tuelsch@gmail.com>
2017-12-06 17:37:01 +0800

    Cleanup

--- config/back.dev.config.js
+++ config/back.dev.config.js
@@ -1,15 +1,8 @@
-const fs = require('fs');
-const path = require('path');
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const backConfig = require('./back.config');
 const paths = require('./paths');
-
-// add hot-reload related code to entry chunks
-Object.keys(backConfig.entry).forEach((name) => {
-  backConfig.entry[name] = ['./build/dev-client'].concat(backConfig.entry[name])
-});
 
 const devConfig = merge(backConfig, {
   output: {
@@ -22,10 +15,9 @@ const devConfig = merge(backConfig, {
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoEmitOnErrorsPlugin(),
     new HtmlWebpackPlugin({
-      filename: 'bolg.html',
+      filename: 'index.html',
       template: paths.backIndex,
       inject: true,
-      serviceWorkerLoader: `<script>${fs.readFileSync(paths.serviceWorkerDev, 'utf-8')}</script>`,
     }),
   ],
   devtool: '#cheap-module-eval-source-map',
