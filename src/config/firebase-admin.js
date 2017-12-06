32c57c1595268b7e91dc26185e040603563ea7fe
Philipp Gfeller <tuelsch@gmail.com>
2017-12-06 17:38:10 +0800

    Check if app is already initialized

--- src/config/firebase-admin.js
+++ src/config/firebase-admin.js
@@ -2,10 +2,12 @@ import admin from 'firebase-admin';
 
 const serviceAccount = require('./firebase-admin-key.json');
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://bolg-d1098.firebaseio.com',
-  storageBucket: 'bolg-d1098.appspot.com',
-});
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://bolg-d1098.firebaseio.com',
+    storageBucket: 'bolg-d1098.appspot.com',
+  });
+}
 
 export default admin;
