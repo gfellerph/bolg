e8db2c50a9f004405ff5bcf9b8ff64dccd025d70
Philipp Gfeller <tuelsch@gmail.com>
2017-12-07 09:46:53 +0800

    Remove unused condition

--- src/post.js
+++ src/post.js
@@ -2,12 +2,11 @@ import mediumZoom from 'src/config/medium-zoom';
 import drawing from 'src/modules/drawing';
 import { setBookmark } from 'src/modules/bookmark';
 
-if (window.location.href.indexOf('bolg.html') <= 0) {
-  window.addEventListener('unload', () => {
-    setBookmark();
-  });
-  window.addEventListener('DOMContentLoaded', () => {
-    mediumZoom();
-    drawing();
-  });
-}
+window.addEventListener('unload', () => {
+  setBookmark();
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+  mediumZoom();
+  drawing();
+});
