222f19ad502cb74046670ee372b064cce1a5d101
Philipp Gfeller <tuelsch@gmail.com>
2017-12-14 23:49:58 +0800

    Use image controller for db and async stuff

--- src/models/Image.js
+++ src/models/Image.js
@@ -1,7 +1,4 @@
 import cuid from 'cuid';
-import { storage, database } from 'src/config/firebase';
-
-const getFileExtension = str => str.substring(str.lastIndexOf('.'), str.length);
 
 export default function Image(img = {}) {
   this.file = img.file || null;
@@ -9,8 +6,6 @@ export default function Image(img = {}) {
   this.thumbnails = img.thumbnails || null;
   this.id = img.id || cuid();
   this.lastModified = img.file ? img.file.lastModified : Date.now();
-
-  const imagesRef = database.ref(`/images/gallery/${this.id}`);
 
   // Methods
   this.url = () => {
@@ -20,17 +15,4 @@ export default function Image(img = {}) {
     return url;
   };
 
-  this.put = () => {
-    if (!this.file) throw new Error('Can\'t upload image, no file specified');
-    if (this.downloadURL) return Promise.resolve({ downloadURL: this.downloadURL });
-
-    return storage
-      .ref(`/gallery/${this.id}${getFileExtension(this.file.name)}`)
-      .put(this.file, { customMetadata: { id: this.id } });
-  };
-
-  this.getThumbnail = width =>
-    new Promise((resolve) => {
-      imagesRef.child(width).once('value', snapshot => resolve(snapshot.val()));
-    });
 }
