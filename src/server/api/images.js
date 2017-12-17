14ca7a54dff283554e7c0d75076d7e2f77399462
Philipp Gfeller <tuelsch@gmail.com>
2017-12-17 15:51:05 +0800

    Rewrite images api to decrease tinify compilations. minify thumbs on server

--- src/server/api/images.js
+++ src/server/api/images.js
@@ -1,50 +1,98 @@
 import tinify from 'tinify';
+import sharp from 'sharp';
 import Image from 'src/models/Image';
 import s3 from 'src/config/s3';
+import imagemin from 'imagemin';
+import imageminPngquant from 'imagemin-pngquant';
+import imageminMozJpeg from 'imagemin-mozjpeg';
+import sizeOf from 'image-size';
 import awsConfig from 'src/config/tinify-aws';
 import { cloudFrontify, sizes } from 'src/config/constants';
 
-export const postImage = (req, res) => {
-  const source = tinify.fromBuffer(req.file.buffer);
+// Function to resize an image based on a sharp object and a size
+const resizer = (sharpImage, size) => sharpImage
+  .resize(size.width, size.height)
+  .min()
+  .toBuffer()
+  .catch((err) => { throw err; })
 
-  const original = source
-    .store(awsConfig(`bolg/images/${req.body.id}`))
+// Minify an image based on a buffer
+const minifier = buffer => imagemin.buffer(buffer, {
+  plugins: [
+    imageminMozJpeg(),
+    imageminPngquant({ quality: '65-80' }),
+  ],
+});
+
+// Filter the sizes collection based on image size
+const filterSizes = imageSize => sizes
+  .filter(size => imageSize.width > size.width || imageSize.height > size.height);
+
+// Promise wrapper for s3 upload to bolg bucket
+const uploader = (Body, Key) => new Promise((resolve, reject) => {
+  s3.putObject({
+    Bucket: 'bolg',
+    Key,
+    Body,
+  }, (err, info) => {
+    if (err) return reject(err);
+    return resolve(info);
+  });
+});
+
+export const postImage = (req, res) => {
+  // Tinify and resize to max 2560w or 1440h
+  const source = tinify
+    .fromBuffer(req.file.buffer)
+    .resize({
+      method: 'fit',
+      width: 2560,
+      height: 1440,
+    });
+
+  // Upload original to s3
+  const originalUploader = source
+    .store(awsConfig(`bolg/i/${req.body.id}`))
     .meta()
-    .then((meta) => {
-      const cloudFrontLink = cloudFrontify(meta.location.split('/bolg/')[1]);
-      const img = new Image();
-      delete img.id;
-      img.downloadURL = cloudFrontLink;
-      return img;
+    .then(() => `i/${req.body.id}`)
+    .catch((err) => { throw err; });
+
+  // Store tinified original as buffer
+  const thumbnailUploader = source.toBuffer()
+    .then((buffer) => {
+      // Get resolution of the image
+      const filteredSizes = filterSizes(sizeOf(buffer));
+      const sharpBuffer = sharp(buffer);
+      return filteredSizes.map(thumbnailSize => resizer(sharpBuffer, thumbnailSize)
+        .then(thumbnail => minifier(thumbnail))
+        .then(thumbnail => uploader(thumbnail, `i/${req.body.id}.${thumbnailSize.width}`))
+        .then(() => ({
+          width: thumbnailSize.width,
+          path: `i/${req.body.id}.${thumbnailSize.width}`,
+        }))
+        .catch((err) => { throw err; }))
+    })
+    .then(promises => Promise.all(promises))
+    .catch((err) => { throw err; })
+
+  // Wait for all uploads to finish
+  return Promise.all([
+    originalUploader,
+    thumbnailUploader,
+  ])
+    .then((paths) => {
+      const img = new Image({ id: req.body.id });
+      const [downloadUrl, thumbnails] = paths;
+      img.downloadURL = cloudFrontify(downloadUrl);
+      img.thumbnails = thumbnails.reduce((acc, thumbnail) => {
+        acc[thumbnail.width] = cloudFrontify(thumbnail.path);
+        return acc;
+      }, {});
+      res.send(img);
     })
     .catch((err) => {
       throw new Error(err);
     });
-
-  const thumbs = Promise.all(sizes.map(size => source.resize({
-    width: size.width,
-  })
-    .store(awsConfig(`bolg/thumbnails/${req.body.id}.${size.width}`))
-    .meta()
-    .then((thumbMeta) => {
-      const thumbLocation = {};
-      thumbLocation[size.width] = cloudFrontify(thumbMeta.location.split('/bolg/')[1]);
-      return thumbLocation;
-    })))
-    .then(thumbsMeta => thumbsMeta.reduce((acc, thumbMeta) => Object.assign(acc, thumbMeta), {}));
-
-  Promise.all([
-    original,
-    thumbs,
-  ])
-    .then((metas) => {
-      const [image, thumbnails] = metas;
-      image.thumbnails = thumbnails;
-      res.send(image);
-    })
-    .catch((err) => {
-      throw new Error(err);
-    })
 };
 
 export const getImage = (req, res) => {
@@ -53,16 +101,15 @@ export const getImage = (req, res) => {
 
 export const deleteImages = (req, res) => {
   const { id } = req.params;
-  const objs = sizes.map(size => ({ Key: `thumbnails/${id}.${size.width}` }));
-  objs.push({ Key: `images/${id}` });
-  const deleter = s3.deleteObjects({
+  const objs = sizes.map(size => ({ Key: `i/${id}.${size.width}` }));
+  objs.push({ Key: `i/${id}` });
+  s3.deleteObjects({
     Bucket: 'bolg',
     Delete: {
       Objects: objs,
     },
+  }, (err, data) => {
+    if (err) throw err;
+    res.send(data);
   });
-  deleter.on('error', (err) => {
-    throw err;
-  });
-  deleter.on('end', data => res.send(data));
 }
