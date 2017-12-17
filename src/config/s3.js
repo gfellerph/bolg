13fa9c0f8574ecfba6633da14f117f31656e2915
Philipp Gfeller <tuelsch@gmail.com>
2017-12-17 15:53:12 +0800

    Use aws-sdk directly instead of s3

--- src/config/s3.js
+++ src/config/s3.js
@@ -1,11 +1,9 @@
-import s3 from 's3';
+import S3 from 'aws-sdk/clients/s3';
 
-const client = s3.createClient({
-  s3Options: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    region: 'eu-central-1',
-  },
+const client = new S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: 'eu-central-1',
 });
 
 export default client;
