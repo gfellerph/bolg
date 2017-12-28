802e3ee24050986263eb3254cd9b25970b7da23d
Philipp Gfeller <tuelsch@gmail.com>
2017-12-28 16:11:51 +0800

    Small updates

--- src/config/constants.js
+++ src/config/constants.js
@@ -10,10 +10,10 @@ export const states = {
   PUBLISHED: 6,
 };
 
-// Random logo
-export const logoURL = () => `/img/bisnaer${Math.ceil(Math.random() * 30, 10) + 1}.PNG`;
-
 export const cloudFrontify = filename => `https://d3ieg3cxah9p4i.cloudfront.net/${filename}`;
+
+// Random logo
+export const logoURL = () => cloudFrontify(`/static/bisnaer${Math.ceil(Math.random() * 30, 10) + 1}.PNG`);
 
 export const mapsAPIKey = 'AIzaSyBADvjevyMmDkHb_xjjh3FOltkO2Oa8iAQ';
 
