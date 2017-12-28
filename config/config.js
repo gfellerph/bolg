9ead3b1790ff1b175d9446868c038933b28fbc94
Philipp Gfeller <tuelsch@gmail.com>
2017-12-28 16:05:22 +0800

    Add socket.io

--- config/config.js
+++ config/config.js
@@ -3,6 +3,7 @@ module.exports = {
     '/publish': { target: 'http://localhost:3000', secure: false },
     '/unpublish': { target: 'http://localhost:3000', secure: false },
     '/api': { target: 'http://localhost:3000', secure: false },
+    '/socket.io': { target: 'ws://localhost:3000', secure: false, ws: true },
   },
   autoOpenBrowser: true,
 }
