73b1b1474665d1527debc600f48e5e649e746850
Philipp Gfeller <tuelsch@gmail.com>
2018-01-06 18:44:17 +0800

    Polyfill array.from (because of medium zoom)

--- src/post.js
+++ src/post.js
@@ -1,6 +1,9 @@
 import mediumZoom from 'src/config/medium-zoom';
 import drawing from 'src/modules/drawing';
+import arrayFrom from 'array.from/polyfill';
 import { setBookmark } from 'src/modules/bookmark';
+
+Array.from = arrayFrom;
 
 window.addEventListener('unload', () => {
   setBookmark();
