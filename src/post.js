0fe4b8ca2b04456e50a340f81fad923bc32598e3
Philipp Gfeller <tuelsch@gmail.com>
2018-01-06 20:08:15 +0800

    Wait for a better solution to address the IE11 problem

--- src/post.js
+++ src/post.js
@@ -1,9 +1,6 @@
 import mediumZoom from 'src/config/medium-zoom';
 import drawing from 'src/modules/drawing';
-import arrayFrom from 'array.from/polyfill';
 import { setBookmark } from 'src/modules/bookmark';
-
-Array.from = arrayFrom();
 
 window.addEventListener('unload', () => {
   setBookmark();
