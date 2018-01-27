2f4ee2358b9e8c91d9771707c5fb383ceda68866
Philipp Gfeller <tuelsch@gmail.com>
2018-01-27 21:04:03 +0800

    Use unified error handler and add comments

--- src/server/api/tipp-api.js
+++ src/server/api/tipp-api.js
@@ -1,54 +1,69 @@
 import Tipp from 'src/models/TippModel';
 
-export const postTipp = (req, res) => {
+/**
+ * Save a new tipp
+ * @param {Object} req Request object
+ * @param {Object} res Response object
+ * @param {Function} next Callback for next middleware in route
+ */
+export const postTipp = (req, res, next) => {
   const tipp = new Tipp(req.body);
 
   tipp.save()
     .then(newTipp => res.json(newTipp))
-    .catch((err) => {
-      res.status = 500;
-      res.send(err.message);
-    });
+    .catch(err => next(err));
 };
 
-export const deleteTipp = (req, res) => {
+/**
+ * Delete a tipp, needs authentication
+ * @param {Object} req Request object
+ * @param {Object} res Response object
+ * @param {Function} next Callback for next middleware in route
+ */
+export const deleteTipp = (req, res, next) => {
   Tipp.remove({
     _id: req.params.id,
   })
     .then(() => res.send('OK'))
-    .catch((err) => {
-      res.status = 500;
-      res.send(err.message);
-    });
+    .catch(err => next(err));
 };
 
-export const putTipp = (req, res) => {
+/**
+ * Update a tipp
+ * @param {Objec} req
+ * @param {Object} res
+ * @param {Function} next Callback for next middleware in route
+ */
+export const putTipp = (req, res, next) => {
   Tipp.findByIdAndUpdate(req.params.id, {
     $set: req.body,
   })
     .then(() => res.send('OK'))
-    .catch((err) => {
-      res.status = 500;
-      res.send(err.message);
-    });
+    .catch(err => next(err));
 };
 
-export const listTipps = (req, res) => {
+/**
+ * Get a list of all tipps
+ * @param {Object} req
+ * @param {Object} res
+ * @param {Function} next Callback for next middleware in route
+ */
+export const listTipps = (req, res, next) => {
   Tipp.find({})
     .then(tipps => res.json(tipps))
-    .catch((err) => {
-      res.status = 500;
-      res.send(err.message);
-    });
+    .catch(err => next(err));
 };
 
-export const getTipp = (req, res) => {
+/**
+ * Get one tipp
+ * @param {Object} req
+ * @param {Object} res
+ * @param {Function} next Callback for next middleware in route
+ */
+export const getTipp = (req, res, next) => {
   Tipp.findOne({
     _id: req.params.id,
   })
     .then(tipp => res.json(tipp))
-    .catch((err) => {
-      res.status = 500;
-      res.send(err.message);
-    });
+    .catch(err => next(err));
 };
