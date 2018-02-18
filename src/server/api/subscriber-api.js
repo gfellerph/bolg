db31815e84cbd2aab1dfadb311126bc2f32066a1
Philipp Gfeller <tuelsch@gmail.com>
2018-02-18 13:59:31 +0800

    Fix lockfile and subscriber api linting

--- src/server/api/subscriber-api.js
+++ src/server/api/subscriber-api.js
@@ -1,38 +1,32 @@
 import SubscriberModel from 'src/models/SubscriberModel';
 
-export const list = (req, res, next) => {
-  return SubscriberModel.find({}, 'id name email')
-    .then(res.json)
-    .catch(next);
-};
+export const list = (req, res, next) => SubscriberModel.find({}, 'id name email')
+  .then(data => res.json(data))
+  .catch(err => next(err));
 
-export const get = (req, res, next) => {
-  return SubscriberModel.findOne({
-    _id: req.params.id,
-  })
-    .then(res.json)
-    .catch(next);
-};
+export const get = (req, res, next) => SubscriberModel.findOne({
+  _id: req.params.id,
+})
+  .then(data => res.json(data))
+  .catch(err => next(err));
 
 export const post = (req, res, next) => {
   const subscriber = new SubscriberModel(req.body);
   return subscriber.save()
-    .then(res.json)
-    .catch(next);
+    .then(data => res.json(data))
+    .catch(err => next(err));
 };
 
-export const put = (req, res, next) => {
-  return SubscriberModel.findByIdAndUpdate(req.params.id, {
-    $set: req.body,
-  })
-    .then(res.json)
-    .catch(next);
-};
+export const put = (req, res, next) => SubscriberModel.findByIdAndUpdate(req.params.id, {
+  $set: req.body,
+}, {
+  new: true,
+})
+  .then(data => res.json(data))
+  .catch(err => next(err));
 
-export const remove = (req, res, next) => {
-  return SubscriberModel.remove({
-    _id: req.params.id,
-  })
-    .then(res.json)
-    .catch(next);
-};
+export const remove = (req, res, next) => SubscriberModel.remove({
+  _id: req.params.id,
+})
+  .then(data => res.json(data))
+  .catch(err => next(err));
