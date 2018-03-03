6dc7831bd099eefe4e12c817ed411b0502057f31
Philipp Gfeller <tuelsch@gmail.com>
2018-03-03 20:22:24 +0900

    Migrate subscribers

--- src/server/routes/migration-routes.js
+++ src/server/routes/migration-routes.js
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import passport from 'passport';
 import * as tipps from 'src/server/migrations/migrate-tipps';
 import * as posts from 'src/server/migrations/migrate-posts';
+import * as subscribers from 'src/server/migrations/migrate-subscribers';
 
 const router = Router();
 
 router.use(passport.authenticate('jwt', { session: false }));
 router.get('/tipps', tipps.get);
 router.get('/posts', posts.get);
+router.get('/subscribers', subscribers.get);
 
 export default router;
