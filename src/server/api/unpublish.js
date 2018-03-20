481dddab2d3c1d02b635c20c37e717aa119d4729
Philipp Gfeller <tuelsch@gmail.com>
2018-03-21 00:03:10 +0900

    Fix index building in existing workflows

--- src/server/api/unpublish.js
+++ src/server/api/unpublish.js
@@ -1,4 +1,6 @@
-import { unpublish, buildIndex } from '../index';
+import { buildIndex } from 'src/server/modules/build';
+import { publishedPosts } from 'src/server/modules/queries';
+import { unpublish } from '../index';
 
 /**
  * Unpublish a previously published blog post, so
@@ -10,7 +12,8 @@ export default function unpublishApi(req, res) {
   res.setHeader('Acces-Control-Allow-Origin', 'localhost');
 
   unpublish(req.params.id)
-    .then(buildIndex)
+    .then(publishedPosts)
+    .then(p => buildIndex(p))
     .then(() => res.send({ message: 'Unpublish complete.' }))
     .catch(err => res.status(500).send(err));
 }
