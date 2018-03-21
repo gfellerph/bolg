b83d1ad09467539536f2ef3014a22e8af63fb88d
Philipp Gfeller <tuelsch@gmail.com>
2018-03-21 18:10:12 +0900

    Refactor auth store, make use of getters and actions

--- src/stores/auth-store.js
+++ src/stores/auth-store.js
@@ -1,36 +1,57 @@
-import User from 'src/models/User';
 import axios from 'axios';
 
 /* eslint no-param-reassign: 0 */
 export default {
   state: {
-    authenticated: false,
-    user: new User(),
-    mongoUser: null,
+    user: null,
   },
   mutations: {
     LOGIN(state, action) {
-      state.authenticated = true;
-      state.user = new User(action.user);
-    },
-
-    LOGOUT(state) {
-      state.authenticated = false;
-      state.user = new User();
-    },
-
-    UPDATE_PROFILE(state, action) {
-      state.user = new User(action.user);
-    },
-
-    MONGOLOGIN(state, action) {
-      state.mongoUser = action;
+      state.user = action.user;
+      window.localStorage.setItem('token', action.token);
       axios.defaults.headers.common.Authorization = `Bearer ${action.token}`;
     },
 
-    MONGOLOGOUT(state) {
-      state.mongoUser = null;
-      window.localStorage.setItem('token', null);
+    LOGOUT(state) {
+      state.user = null;
+      window.localStorage.setItem('token', '');
+      axios.defaults.headers.common.Authorization = '';
+    },
+  },
+  getters: {
+    authenticated: state => !!state.user,
+  },
+  actions: {
+    LOGIN({ commit }, credentials) {
+      return axios.post('/api/user/authenticate', {
+        email: credentials.email,
+        password: credentials.password,
+      })
+        .then((res) => {
+          commit('LOGIN', {
+            user: res.data.user,
+            token: res.data.token,
+          });
+        })
+    },
+
+    REFRESH({ commit }) {
+      const token = window.localStorage.getItem('token');
+      if (!token) return Promise.reject();
+      return axios.get('/api/user', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+        .then((res) => {
+          commit('LOGIN', {
+            token,
+            user: res.data,
+          });
+        })
+        .catch(() => {
+          commit('LOGOUT')
+        });
     },
   },
 };
