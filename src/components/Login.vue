856c7d79bf5190d96f704ac18c6565f08e6ba030
Philipp Gfeller <tuelsch@gmail.com>
2018-03-21 18:10:43 +0900

    Make use of store getters and actions

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,28 +1,23 @@
 <template>
   <div class="login">
-    <auth-guard>
-      <router-link slot="auth" to="/">Home</router-link>
-      <div slot="no-auth" @keyup.enter="login">
-        <p>
-          <label for="email">Email</label>
-          <input id="email" v-model="email" type="text">
-        </p>
-        <p>
-          <label for="password">Password</label>
-          <input id="password" v-model="password" type="password">
-        </p>
-        <p class="error" v-if="error">{{error}}</p>
-        <p><button :disabled="loading" @click="login">Login</button></p>
-      </div>
-    </auth-guard>
+    <router-link v-if="$store.state.auth.mongoUser" to="/">Home</router-link>
+    <div v-if="!$store.state.auth.mongoUser" @keyup.enter="login">
+      <p>
+        <label for="email">Email</label>
+        <input id="email" v-model="email" type="text">
+      </p>
+      <p>
+        <label for="password">Password</label>
+        <input id="password" v-model="password" type="password">
+      </p>
+      <p class="error" v-if="error">{{error}}</p>
+      <p><button :disabled="loading" @click="login">Login</button></p>
+    </div>
   </div>
 </template>
 
 <script>
-  import User from 'src/models/UserAdmin';
   import router from 'src/config/router';
-  import AuthGuard from 'src/components/AuthGuard';
-  import { auth } from 'src/config/firebase';
 
   export default {
     data() {
@@ -34,35 +29,44 @@
       };
     },
 
-    created() {
-      auth.onAuthStateChanged((user) => {
-        if (user) {
-          if (this.$route.query.redirect) {
-            router.push({ path: this.$route.query.redirect });
-          }
-        } else {
-          router.push({ path: '/' })
-        }
-      });
+    // Test if token is still valid
+    mounted() {
+      // Prevent user from submitting temporarily
+      this.loading = true;
+      this.$store.dispatch('REFRESH')
+        .then(() => {
+          this.loading = false;
+          this.reroute();
+        })
+        .catch(() => {
+          this.loading = false;
+        });
     },
 
     methods: {
+      reroute() {
+        if (this.$route.query.redirect) {
+          router.push({ path: this.$route.query.redirect });
+        } else {
+          router.push({ path: '/' })
+        }
+      },
       login() {
         this.loading = true;
-        new User().login(this.email, this.password)
+        this.$store.dispatch('LOGIN', {
+          email: this.email,
+          password: this.password,
+        })
           .then(() => {
             this.loading = false;
             this.error = false;
+            this.reroute();
           })
           .catch((err) => {
             this.loading = false;
             this.error = err.message;
           });
       },
-    },
-
-    components: {
-      AuthGuard,
     },
   };
 </script>
