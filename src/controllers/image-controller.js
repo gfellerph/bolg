fb17d6a22f10b48b30db8669aaa9d1d57b9e73b5
Philipp Gfeller <tuelsch@gmail.com>
2018-04-05 21:17:27 +0900

    Use shortid to store the image

--- src/controllers/image-controller.js
+++ src/controllers/image-controller.js
@@ -4,11 +4,11 @@ export default () => {
   const upload = (image, config) => {
     const formData = new FormData();
     formData.append('image', image.file);
-    formData.append('id', image.id);
-    return axios.post('/api/images', formData, config);
+    formData.append('id', image.shortid);
+    return axios.post('/api/image', formData, config);
   }
 
-  const deleteImages = id => axios.delete(`/api/images/${id}`);
+  const deleteImages = id => axios.delete(`/api/image/${id}`);
 
   return {
     upload,
