63692502e5fc0a3a94d38e50500fa0af905ec6bd
Philipp Gfeller <tuelsch@gmail.com>
2018-04-07 19:21:18 +0900

    Add relevant api endpoints

--- src/stores/posts-store.js
+++ src/stores/posts-store.js
@@ -5,12 +5,26 @@ export default {
     posts: [],
   },
   mutations: {
-    POSTS: (state, action) => { state.posts = action.posts },
+    POSTS_UPDATE: (state, posts) => { state.posts = posts },
+    POSTS_UPDATE_ONE: (state, newPost) => {
+      /* eslint no-confusing-arrow: 0 */
+      state.posts = state.posts.map(post => newPost._id === post._id ? newPost : post);
+    },
+    POSTS_DELETE: (state, id) => { state.posts = state.posts.filter(post => post._id !== id); },
   },
   actions: {
     GETPOSTS: async ({ commit }) => {
       const res = await axios.get('/api/posts');
-      commit('POSTS', { posts: res.data });
+      commit('POSTS_UPDATE', res.data);
+    },
+    POSTS_PUBLISH: async ({ commit }, id) => {
+      const res = await axios.get(`/api/publish/${id}`);
+      commit('POSTS_UPDATE_ONE', res.data);
+    },
+    POSTS_DELETE_ONE: async ({ commit }, id) => {
+      const res = await axios.delete(`/api/post/${id}`);
+      commit('POSTS_DELETE', id);
+      return res;
     },
   },
 }
