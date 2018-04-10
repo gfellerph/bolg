ae614bad9bcaf299daf950efb4dd073e18891acc
Philipp Gfeller <tuelsch@gmail.com>
2018-04-10 16:29:09 +0900

    Refactor srcset generator function

--- src/models/ImageModel.js
+++ src/models/ImageModel.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import shortid from 'shortid';
+import { getThumbUrl, getSrcset } from 'src/config/constants';
 
 const { Schema } = mongoose;
 
@@ -16,6 +17,14 @@ export const ImageSchema = new Schema({
     type: String,
     default: shortid.generate,
   },
+});
+
+ImageSchema.virtual('thumbUrl').get(function thumbUrl() {
+  return getThumbUrl(this.url, 640);
+});
+
+ImageSchema.virtual('srcset').get(function srcset() {
+  return getSrcset(this.url);
 });
 
 export default mongoose.model('Image', ImageSchema);
