e3e9d0a1ee2bc00db32d4edf4c6ebe1c95ba81bb
Philipp Gfeller <tuelsch@gmail.com>
2018-04-11 16:24:17 +0900

    Update medium-zoom to be able to exclude heavy polyfills

--- src/post.js
+++ src/post.js
@@ -1,7 +1,5 @@
 import mediumZoom from 'src/config/medium-zoom';
 import drawing from 'src/modules/drawing';
-// TODO: Remove babel-polyfill as soon as #34 on medium-zoom gets resolved!
-import 'babel-polyfill';
 import { setBookmark } from 'src/modules/bookmark';
 
 window.addEventListener('unload', () => {
