0ad015e2a7949b6f39c7336db5f9ec4ef3fc7466
Philipp Gfeller <tuelsch@gmail.com>
2018-04-11 16:24:30 +0900

    Strip comments from vue distribution

--- config/front.prod.config.js
+++ config/front.prod.config.js
@@ -11,6 +11,11 @@ const prodConfig = merge(frontConfig, {
     filename: 'js/[name].[chunkhash].js',
   },
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      },
+    }),
     new ExtractTextPlugin({
       filename: 'css/[name].[chunkhash].css',
       allChunks: true,
