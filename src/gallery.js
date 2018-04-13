66b16b2a5dfb303bf45c180cb6535c2be09fcb00
Philipp Gfeller <tuelsch@gmail.com>
2018-04-13 23:03:18 +0900

    init lqip

--- src/gallery.js
+++ src/gallery.js
@@ -1,6 +1,8 @@
 import mediumZoom from 'src/config/medium-zoom';
+import lqip from 'src/modules/lqip';
 
 window.addEventListener('DOMContentLoaded', () => {
   // Run medium zoom
   mediumZoom();
+  lqip();
 });
