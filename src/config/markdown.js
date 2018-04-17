30ee6a99c08f4399406231cdfa8805576038009a
Philipp Gfeller <tuelsch@gmail.com>
2018-04-17 21:43:29 +0900

    Use lqip in posts (no progressive image, missing ratio)

--- src/config/markdown.js
+++ src/config/markdown.js
@@ -1,5 +1,5 @@
 import markdownParser from 'marked';
-import { getSrcset } from 'src/config/constants';
+import { getSrcset, constructThumborUrl } from 'src/config/constants';
 
 const markdownOptions = {
   gfm: true,
@@ -10,16 +10,38 @@ export const marked = (str, options) => {
   const renderer = new markdownParser.Renderer();
   renderer.image = (href, title, text) => {
     let srcset = false;
+    let lqipSrc = false;
+    let noZoom = false;
+    let src = href;
     // Filter self hosted images as well as jpgs and pngs, no gifs
     if (href.indexOf('//adie.bisnaer.ch/') >= 0) {
+      /* eslint no-param-reassign: 0 */
+      lqipSrc = href;
+      noZoom = true;
+      src = constructThumborUrl(href, {
+        width: 20,
+        filters: {
+          blur: 2,
+        },
+      });
       srcset = getSrcset(href);
     }
-    const hrefAttr = href ? ` src="${href}"` : '';
+    const srcAttr = href ? ` src="${src}"` : '';
     const titleAttr = title ? ` title="${title}"` : '';
     const altAttr = text ? ` alt="${text}"` : '';
     const srcsetAttr = srcset ? ` srcset="${srcset}"` : '';
+    const lqipSrcsetAttr = srcset ? ` data-lqip-srcset="${srcset}"` : '';
+    const lqipSrcAttr = lqipSrc ? ` data-lqip-src="${lqipSrc}"` : '';
     const sizesAttr = srcset ? ' sizes="(max-width: 640px) 100vw, 640px"' : '';
-    return `<img${hrefAttr}${titleAttr}${altAttr}${srcsetAttr}${sizesAttr}>`;
+    const noZoomAttr = noZoom ? ' data-no-zoom' : '';
+    return `
+      <div class="lqip__wrapper">
+        <img${srcAttr}${titleAttr}${altAttr}${lqipSrcsetAttr}${sizesAttr}${lqipSrcAttr}${noZoomAttr}>
+        <noscript>
+          <img src="${href}"${titleAttr}${altAttr}${srcsetAttr}${sizesAttr}${lqipSrcAttr}>
+        </noscript>
+      </div>
+    `;
   }
   const mergedOptions = Object.assign({}, markdownOptions, options);
   mergedOptions.renderer = renderer;
--- src/post.js
+++ src/post.js
@@ -1,5 +1,6 @@
 import mediumZoom from 'src/config/medium-zoom';
 import drawing from 'src/modules/drawing';
+import lqip from 'src/modules/lqip';
 import { setBookmark } from 'src/modules/bookmark';
 
 window.addEventListener('unload', () => {
@@ -7,6 +8,13 @@ window.addEventListener('unload', () => {
 });
 
 window.addEventListener('DOMContentLoaded', () => {
+  // Zoom every image but lqips
   mediumZoom();
   drawing();
+  lqip({
+    afterReplace: (placeholder, original) => {
+      // Add zoom to original lqips
+      mediumZoom(original);
+    },
+  })
 });
