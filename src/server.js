4d1602610fd4f20f7e78dccdcaffeabc2a41b467
Philipp Gfeller <tuelsch@gmail.com>
2018-04-19 14:46:38 +0900

    delete unused files from src (mostly firebase)

--- src/server.js
+++ src/server.js
@@ -8,7 +8,6 @@ import mongoose from 'mongoose';
 import io from 'socket.io';
 import redirectRoutes from 'src/server/routes/redirect-routes';
 import apiRoutes from 'src/server/routes/api-routes';
-import migrationRoutes from 'src/server/routes/migration-routes';
 import errorRoutes from 'src/server/routes/error-routes';
 import passport from 'src/config/passport';
 import expressStatic from 'src/config/express-static';
@@ -50,7 +49,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Routes
 app.use(redirectRoutes);
 app.use('/api', apiRoutes);
-app.use('/migrate', migrationRoutes);
 app.use(errorRoutes);
 
 export default app;
