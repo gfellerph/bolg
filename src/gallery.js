580f0e28f187f6e09add0dc7cb9de0a97a5cfa3c
Philipp Gfeller <philipp.gfeller@maxomedia.ch>
2018-07-01 17:47:59 +0900

    Remove comment

--- src/gallery.js
+++ src/gallery.js
@@ -2,8 +2,6 @@ import mediumZoom from 'src/config/medium-zoom';
 import lqip from 'src/modules/lqip';
 
 window.addEventListener('DOMContentLoaded', () => {
-  // Run medium zoom
-  // mediumZoom();
   lqip({
     afterReplace: (lqipImage, originalImage) => {
       if (lqipImage) lqipImage.remove();
