0bd7b5252e71c17806f67e0afb8040ed572b0ad3
Philipp Gfeller <philipp.gfeller@maxomedia.ch>
2018-07-01 17:50:04 +0900

    No log

--- src/server/migration/fix-ratio.js
+++ src/server/migration/fix-ratio.js
@@ -5,7 +5,6 @@ export default function (req, res) {
     .then((posts) => {
       const images = posts.reduce((imgAcc, post) => imgAcc.concat(post.images), []);
       const imgsWithoutRatio = images.filter(img => img.ratio === undefined);
-      console.log(imgsWithoutRatio.length, Object.keys(imgsWithoutRatio[0]))
       return Promise.all(imgsWithoutRatio.map(async (img) => {
         const ratio = await img.getRatio();
         img.set({ ratio });
