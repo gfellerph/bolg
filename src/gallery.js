677fa6a1ed92775a77a5f5ed1a95697d49791bba
Philipp Gfeller <philipp.gfeller@maxomedia.ch>
2018-07-11 17:43:38 +0800

    Add lazy loader script to gallery bundle and initialize it

--- src/gallery.js
+++ src/gallery.js
@@ -1,7 +1,19 @@
 import mediumZoom from 'src/config/medium-zoom';
 import lqip from 'src/modules/lqip';
+import galleryLoader from 'src/modules/gallery-lazy-load';
 
 window.addEventListener('DOMContentLoaded', () => {
+  galleryLoader({
+    afterInsert(lastPost) {
+      lqip({
+        selectorRoot: lastPost,
+        afterReplace: (lqipImage, originalImage) => {
+          if (lqipImage) lqipImage.remove();
+          mediumZoom(originalImage);
+        },
+      })
+    },
+  });
   lqip({
     afterReplace: (lqipImage, originalImage) => {
       if (lqipImage) lqipImage.remove();
