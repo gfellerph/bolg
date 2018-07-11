81713ce1ad6ed20505d7bd6c57d404f68c971f43
Philipp Gfeller <philipp.gfeller@maxomedia.ch>
2018-07-11 15:56:38 +0800

    Handle paged requests differently than get-all requests

--- src/server/api/gallery-api.js
+++ src/server/api/gallery-api.js
@@ -2,14 +2,25 @@ import Post from 'src/models/PostModel';
 import { buildGalleryPost } from 'src/server/modules/build';
 
 export default (req, res, next) => {
-  Post.paginate({}, {
-    page: req.query.page,
-    limit: req.query.limit,
-    sort: '-postDate',
-    select: 'title images postDate',
-  })
+  let dataRequest;
+  if (req.query.page) {
+    dataRequest = Post.paginate({}, {
+      page: req.query.page,
+      limit: req.query.limit,
+      sort: '-postDate',
+      select: 'title images postDate',
+    })
+      .then(data => data.docs)
+  } else {
+    dataRequest = Post
+      .find({})
+      .sort('-postDate')
+      .select('title images postDate')
+      .lean();
+  }
+  dataRequest
     .then((data) => {
-      const posts = data.docs.map(post => buildGalleryPost(post));
+      const posts = data.map(post => buildGalleryPost(post));
       res.send(posts.join());
     })
     .catch(err => next(err));
