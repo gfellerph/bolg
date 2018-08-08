76b6be0e5522d9af135d951854f2b02bdc032184
Philipp Gfeller <philipp.gfeller@maxomedia.ch>
2018-08-08 14:53:18 +0800

    Add another drawing border

--- src/components/CreateDrawing.vue
+++ src/components/CreateDrawing.vue
@@ -2,7 +2,9 @@
   <div class="drawing" id="zeichne">
     <hr class="blog-divider">
     <p class="italic text-align-center">Zeichne dini Meinig zur Gschicht:</p>
-    <canvas id="drawing" class="drawing__canvas blank"></canvas>
+    <div class="drawing__canvas-border">
+      <canvas id="drawing" class="drawing__canvas blank"></canvas>
+    </div>
     <p class="drawing__error error hide"></p>
     <p class="text-align-center">
       <button class="drawing__clear">Lösche</button>
