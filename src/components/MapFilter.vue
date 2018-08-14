4d5c07669593e8537c47b8d61ecd960f64877235
Philipp Gfeller <philipp.gfeller@maxomedia.ch>
2018-08-14 21:47:32 +0800

    Make the filter interact with the store directly, change info to chkbox

--- src/components/MapFilter.vue
+++ src/components/MapFilter.vue
@@ -1,40 +1,50 @@
 <template>
   <div class="map__filter">
-    <label for="tipps-radio">
-      <input
-        id="tipps-radio"
-        type="radio"
-        name="map-filter"
-        value="tipps"
-        v-model="filter"
-        @input="input"
+    <div class="map__filter-panel">
+      <label
+        for="tipps-radio"
+        :class="{ active: filter === 'tipps' }"
       >
-      <img src="/img/inuksuk.svg" alt="Inuksuk">
-      <span>Tipps</span>
-    </label>
-    <label for="journey-radio">
-      <input
-        id="journey-radio"
-        type="radio"
-        name="map-filter"
-        value="journey"
-        v-model="filter"
-        @input="input"
+        <input
+          id="tipps-radio"
+          type="radio"
+          name="map-filter"
+          value="tipps"
+          v-model="filter"
+        >
+        <img src="/img/inuksuk.svg" alt="Inuksuk">
+        <span>Tipps</span>
+      </label>
+      <label
+        for="journey-radio"
+        :class="{ active: filter === 'journey' }"
       >
-      <span>Tipps</span>
-    </label>
-    <label for="info-radio">
-      <input
-        id="info-radio"
-        type="radio"
-        name="map-filter"
-        value="info"
-        v-model="filter"
-        @input="input"
+        <input
+          id="journey-radio"
+          type="radio"
+          name="map-filter"
+          value="journey"
+          v-model="filter"
+        >
+        <img src="/img/journey.svg" alt="Reiseroute">
+        <span>Route</span>
+      </label>
+    </div>
+    <div class="map__filter-panel">
+      <label
+        for="tipp-info"
+        :class="{ checked: info }"
       >
-      <span v-if="filter!=='info'">Hä?</span>
-      <span v-else>Aha!</span>
-    </label>
+        <input
+          id="tipp-info"
+          type="checkbox"
+          name="tipp-info"
+          v-model="info"
+        >
+        <span v-if="!info">Hä?</span>
+        <span v-else>Aha!</span>
+      </label>
+    </div>
   </div>
 </template>
 
@@ -42,16 +52,26 @@
 import { mapMutations } from 'vuex';
 
 export default {
-  data() {
-    return {
-      filter: 'tipps',
-    };
+  computed: {
+    info: {
+      get() {
+        return this.$store.state.mapStore.info;
+      },
+      set(value) {
+        this.UPDATE_INFO({ info: value });
+      },
+    },
+    filter: {
+      get() {
+        return this.$store.state.mapStore.filter;
+      },
+      set(value) {
+        this.UPDATE_FILTER({ filter: value });
+      },
+    },
   },
   methods: {
-    input(event) {
-      this.UPDATE_FILTER({ filter: event.target.value });
-    },
-    ...mapMutations(['UPDATE_FILTER']),
+    ...mapMutations(['UPDATE_FILTER', 'UPDATE_INFO']),
   },
 }
 </script>
