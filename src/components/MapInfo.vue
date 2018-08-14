b6b544b77f2a1a2a077487075f4d7de19ad61c4a
Philipp Gfeller <philipp.gfeller@maxomedia.ch>
2018-08-14 21:48:15 +0800

    Rewrite text, remove close button

---
+++ src/components/MapInfo.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="tipp-info box">
+    <h2 class="h4">Hesch e Reisetipp?</h2>
+    <p class="small">Weisch es feins Restaurant, es tolls Land oder eifach es schöns Plätzli? Mir si geng uf der Suechi nach guete Reisetipps, egau wo uf dere Wäut. Suech di Ort obe ir Box<span class="hidden-xs"> oder klick uf d'Charte</span> und gibnis di Reisetipp!</p>
+  </div>
+</template>
+
+<style lang="scss">
+  @import 'src/styles/core/_index';
+  @import 'src/styles/atoms/convenience';
+
+  .tipp-info {
+    position: relative;
+    padding: $golden-rem/2;
+    border-top: 1px solid lightgrey;
+  }
+</style>
