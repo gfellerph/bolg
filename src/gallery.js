cbfde101f858e42852b699b71a98181e8af0106a
Philipp Gfeller <philipp.gfeller@maxomedia.ch>
2018-12-22 19:36:18 +0700

    Simplify code

--- src/gallery.js
+++ src/gallery.js
@@ -3,23 +3,22 @@ import mediumZoom from 'src/config/medium-zoom';
 import lqip from 'src/modules/lqip';
 import galleryLoader from 'src/modules/gallery-lazy-load';
 
+const afterReplace = (lqipImage, originalImage) => {
+  if (lqipImage) lqipImage.remove();
+  mediumZoom(originalImage);
+};
+
 window.addEventListener('DOMContentLoaded', () => {
   galleryLoader({
     afterInsert(lastPost) {
       lqip({
         selectorRoot: lastPost,
         rootMargin: '0px',
-        afterReplace: (lqipImage, originalImage) => {
-          if (lqipImage) lqipImage.remove();
-          mediumZoom(originalImage);
-        },
+        afterReplace,
       })
     },
   });
   lqip({
-    afterReplace: (lqipImage, originalImage) => {
-      if (lqipImage) lqipImage.remove();
-      mediumZoom(originalImage);
-    },
+    afterReplace,
   });
 });
